0405d9740cd63c7a6d174f4fc60514520c4952b3
socconlonton2017 <socconlonton2017@gmail.com>
2017-03-27 23:25:16 +0700

    chapter 3 - vult

--- ocajpdev/src/main/java/com/vult/chap3/LogicExpressionDemo.java
+++ ocajpdev/src/main/java/com/vult/chap3/LogicExpressionDemo.java
@@ -4,7 +4,7 @@ public class LogicExpressionDemo {
 	public static void main(String[] args) {
 		LogicExpressionDemo demo = new LogicExpressionDemo();
 
-		boolean b = demo.isTrue("param 1") || demo.isFalse("param 2") && demo.isFalse("param 3") | demo.isFalse("param 4");
+		boolean b = demo.isTrue("param 1") || demo.isFalse("param 2") && demo.isFalse("param 3");
 		System.out.println("Expression is " + b);
 	}
 	
